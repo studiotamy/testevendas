cfc6119995065b86df682a59a67f10279d4fc64b
studiotamy <tamy.designergrafico@gmail.com>
2024-08-22 18:19:02 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('buy-button').addEventListener('click', function() {
-    // Redirecionar para uma plataforma de venda
-    window.location.href = 'https://link-para-plataforma-de-venda.com';
-});
