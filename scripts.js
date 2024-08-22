a08aa07026c785771b94ba4266c73c1708a6bea8
studiotamy <tamy.designergrafico@gmail.com>
2024-08-22 17:57:28 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('buy-button').addEventListener('click', function() {
+    // Redirecionar para uma plataforma de venda
+    window.location.href = 'https://link-para-plataforma-de-venda.com';
+});
